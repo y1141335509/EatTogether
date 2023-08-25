6cc87e29ae71c9c62a71cc59137039100af6ac6e
iyutpo <Y15653862659@outlook.com>
2023-08-24 21:33:40 -0700

    make side bar clickable

--- eat-together/src/components/HomePage.js
+++ eat-together/src/components/HomePage.js
@@ -1,46 +1,97 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import '../index.css';
+import { useState } from "react";
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Layout, Menu, Switch, theme } from 'antd';
+import UserProfile from './UserProfile';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import FoodPreference from './FoodPreference';
+
+
 const { Header, Content, Footer, Sider } = Layout;
 
 
-
+// Define the Main Navigation Page names (on the header bar)
 const nav_labels = {
     '1': 'Home',
     '2': 'Start a Dinner',
     '3': 'Search', 
 }
+// Assign the Main Navigation Page names
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: nav_labels[key],
+}));
+
+
+// Define the Sub Navigation Page names (on the left-side bar)
 const subnav_labels = {
     '1': 'Profile',
     '2': 'Manage Events', 
     '3': 'Social Media', 
 }
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: nav_labels[key],
-}));
+const subnav_profile_options = {
+    '1': 'User Profile',
+    '2': 'Food Preference',
+    '3': 'Saved Restaurants',
+}
+
 const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
   const key = String(index + 1);
   return {
-    key: `sub${key}`,
+    key: key,
     icon: React.createElement(icon),
     label: subnav_labels[key],
-    children: new Array(3).fill(null).map((_, j) => {
-      const subKey = index * 3 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
+    // label: (
+    //     <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+    //         {subnav_labels[key]}
+    //     </a>
+    // ),
+    component: <UserProfile />, 
   };
 });
 
+
+const items = [{
+        key: '1',
+        icon: React.createElement(UserOutlined), 
+        label: (
+                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                    {subnav_labels['1']}
+                </a>
+            ),
+    },     
+    {
+        key: '2',
+        icon: React.createElement(LaptopOutlined), 
+        label: (
+                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                    {subnav_labels['2']}
+                </a>
+            ),
+    },     
+    {
+        key: '3',
+        icon: React.createElement(NotificationOutlined), 
+        label: (
+                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                    {subnav_labels['3']}
+                </a>
+            ),
+    }
+];
+
+
+// Define the functional component
 const HomePage = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const [activeMenuItem, setActiveMenuItem] = useState('1');
+
+
   return (
     <Layout>
       <Header
@@ -57,15 +108,7 @@ const HomePage = () => {
           padding: '0 50px',
         }}
       >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
-          }}
-        >
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          {/* <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item> */}
-        </Breadcrumb>
+
         <Layout
           style={{
             padding: '24px 0',
@@ -79,22 +122,30 @@ const HomePage = () => {
             width={200}
           >
             <Menu
+              onClick={(e) => setActiveMenuItem(e.key)}
               mode="inline"
               defaultSelectedKeys={['1']}
               defaultOpenKeys={['sub1']}
               style={{
                 height: '100%',
               }}
+            //   items={items}
               items={items2}
             />
           </Sider>
+
+          {/* Content on the right hand side of the main page */}
           <Content
             style={{
               padding: '0 24px',
               minHeight: 280,
             }}
           >
-            <h3>Name: </h3>
+            
+            {activeMenuItem === '1' && <UserProfile /> }
+            {activeMenuItem === '2' && <FoodPreference /> }
+            {/* {activeMenuItem === '3' && <SocialMedia /> } */}
+
           </Content>
         </Layout>
       </Content>
@@ -108,37 +159,16 @@ const HomePage = () => {
     </Layout>
   );
 };
+
+
+
+
 export default HomePage;
 
 
 
 
 
-// function HomePage() {
-
-
-    // // define handlers
-    // const handleLogin = (event) => {
-    //     console.log('Login successfully~');
-    // }
-
-
-    // const handleSignup = (event) => {
-    //     console.log('Sign up successfully~');
-    // }
-
-
-    // // return page:
-    // return (
-    //     <div>
-    //         <h1>Welcome to our website</h1>
-    //         <button onClick={handleLogin}>Login</button>
-    //         <button onClick={handleSignup}>Sign Up</button>
-    //     </div>
-    // )
-// }
-
-// export default HomePage;
 
 
 
